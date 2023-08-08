711a7eb7d02fd0fdfe86d59c392911d857eefe53
Kacper Wojda <wojda.kacper@gmail.com>
2023-08-08 22:56:09 +0200

    js refactoring to include functions

--- js/script.js
+++ js/script.js
@@ -1,67 +1,72 @@
-let button = document.querySelector(".form__button");
-let form = document.querySelector(".js-form")
-let result = document.querySelector(".js-result")
+{
+    const result = document.querySelector(".js-result")
+    const currencyOutputElement = document.querySelector(".js-currency-output");
 
-form.addEventListener("input", () => {
-    result.innerText = "Wynik:"
-})
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = document.querySelector(".js-amount");
-    let currencyInput = document.querySelector(".js-currency-input");
-    let currencyOutput = document.querySelector(".js-currency-output");
-    let exchangeRate = 0;
-
-    switch (currencyInput.value) {
-        case "PLN":
-            switch (currencyOutput.value) {
-                case "PLN":
-                    exchangeRate = 1;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "USD":
-                    exchangeRate = 0.25;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "EUR":
-                    exchangeRate = 0.22;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-            }
-            break;
-        case "USD":
-            switch (currencyOutput.value) {
-                case "PLN":
-                    exchangeRate = 4.01;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "USD":
-                    exchangeRate = 1;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "EUR":
-                    exchangeRate = 0.90;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-            }
-            break;
-        case "EUR":
-            switch (currencyOutput.value) {
-                case "PLN":
-                    exchangeRate = 4.45;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "USD":
-                    exchangeRate = 1.11;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-                case "EUR":
-                    exchangeRate = 1;
-                    result.innerText = "Wynik: " + (+amount.value * exchangeRate).toFixed(2) + " " + currencyOutput.value;
-                    break;
-            }
-            break;
+    const showResult = (currencyOutput) => {
+        result.innerText = "Wynik: " + calculateResult(currencyOutputElement.value).toFixed(2) + " " + currencyOutput;
     }
-});
+
+    const calculateResult = (currencyOutput) => {
+        let exchangeRate;
+        const amountElement = document.querySelector(".js-amount");
+        const currencyInputElement = document.querySelector(".js-currency-input");
+
+        switch (currencyInputElement.value) {
+            case "PLN":
+                switch (currencyOutput) {
+                    case "PLN":
+                        exchangeRate = 1;
+                        return (+amountElement.value * exchangeRate);
+                    case "USD":
+                        exchangeRate = 0.25;
+                        return (+amountElement.value * exchangeRate);
+                    case "EUR":
+                        exchangeRate = 0.22;
+                        return (+amountElement.value * exchangeRate);
+                }
+            case "USD":
+                switch (currencyOutput) {
+                    case "PLN":
+                        exchangeRate = 4.01;
+                        return (+amountElement.value * exchangeRate);
+                    case "USD":
+                        exchangeRate = 1;
+                        return (+amountElement.value * exchangeRate);
+                    case "EUR":
+                        exchangeRate = 0.90;
+                        return (+amountElement.value * exchangeRate);
+                }
+            case "EUR":
+                switch (currencyOutput) {
+                    case "PLN":
+                        exchangeRate = 4.45;
+                        return (+amountElement.value * exchangeRate);
+                    case "USD":
+                        exchangeRate = 1.11;
+                        return (+amountElement.value * exchangeRate);
+                    case "EUR":
+                        exchangeRate = 1;
+                        return (+amountElement.value * exchangeRate);
+                }
+        }
+    }
+
+    const onFormInput = () => result.innerText = "Wynik:";
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        calculateResult(currencyOutputElement.value);
+        showResult(currencyOutputElement.value);
+    };
+
+    const init = () => {
+        const form = document.querySelector(".js-form")
+
+        form.addEventListener("input", onFormInput);
+
+        form.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
